d95e0515339e7efd7910f12d0d488d89530e8f56
Alexandre Oliveira <alexandre.yukon@gmail.com>
2025-03-04 15:54:50 -0500

    Support-tickets - add files

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import { parseRoutePath } from '../utils/parseRoutPath.js'
+import { tickets } from './tickets.js'
+
+export const routes = [...tickets].map((route) => ({
+  ...route,
+  path: parseRoutePath(route.path)
+}))
